8f2d8735c65b97ee4931b97dc0970481cc0a127c
malegecha <malegecha@gmail.com>
2014-12-07 10:41:34 +1100

    Course project 1 done

---
+++ 3. Exploratory Data Analysis/course project 1/plot3.R
@@ -0,0 +1,21 @@
+library("data.table")
+plot3 = function() {
+    ## read required data
+    data = fread("household_power_consumption.txt", sep=';', skip = 66637,
+                 select = c(1:2,7,8,9), na.strings="?",  nrows = 2880)
+    ## set column name
+    setnames(data, c(1:5), c("Date","Time","Sub_metering_1",
+                             "Sub_metering_2", "Sub_metering_3"))
+    ## convert data, create new column "POSIXTime"
+    data$Date <- as.Date(data$Date, format="%d/%m/%Y") 
+    data$POSIXTime = as.POSIXct(paste(data$Date, data$Time))
+    # plot data
+    png(filename = "plot3.png")
+        plot(data$Sub_metering_1~data$POSIXTime, type="l",
+             ylab="Energy sub metering(watt-hour of active energy)", xlab="")
+        lines(data$Sub_metering_2~data$POSIXTime,col='Red')
+        lines(data$Sub_metering_3~data$POSIXTime,col='Blue')
+    legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, 
+           legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+    dev.off()
+}
---
+++ 3. Exploratory Data Analysis/course project 1/plot4.R
@@ -0,0 +1,33 @@
+library("data.table")
+plot4 = function() {
+    ## read required data
+    data = fread("household_power_consumption.txt", sep=';', skip = 66637,
+                na.strings="?",  nrows = 2880)
+    ## set column name
+    setnames(data, c(1:9), c("Date", "Time", "Global_active_power",
+                             "Global_reactive_power", "Voltage", 
+                             "Global_intensity","Sub_metering_1",
+                             "Sub_metering_2", "Sub_metering_3"))
+    ## convert data, create new column "POSIXTime"
+    data$Date <- as.Date(data$Date, format="%d/%m/%Y") 
+    data$POSIXTime = as.POSIXct(paste(data$Date, data$Time))
+    # plot data
+    png(filename = "plot4.png")
+    par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0))
+    with(data, {
+        plot(Global_active_power~POSIXTime, type="l", 
+             ylab="Global Active Power (kilowatts)", xlab="")
+        plot(Voltage~POSIXTime, type="l", 
+             ylab="Voltage (volt)", xlab="")
+        plot(Sub_metering_1~POSIXTime, type="l", 
+             ylab="Energy sub metering (watt-hour of active energy)", xlab="")
+        lines(Sub_metering_2~POSIXTime,col='Red')
+        lines(Sub_metering_3~POSIXTime,col='Blue')
+        legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n",
+               legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+        plot(Global_reactive_power~POSIXTime, type="l", 
+             ylab="Global Rective Power (kilowatts)",xlab="")
+    })
+    
+    dev.off()
+}
